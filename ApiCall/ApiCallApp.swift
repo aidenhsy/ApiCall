17ed13a61c85fb7432eae52941062453ae1592f8
aidenhsy <aidenhsy@hotmail.com>
2023-11-12 08:00:56 +0800

    Initial Commit

---
+++ ApiCall/ApiCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiCallApp.swift
+//  ApiCall
+//
+//  Created by Aiden Yang on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct ApiCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
